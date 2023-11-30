2f8b3058ea221eadf0b9b351966dc6fad024d288
Artur Ceschin <artur.ceschin@gmail.com>
2023-11-30 17:33:14 -0300

    feat: 🎸 initial configuration and database settings

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import crypto from 'node:crypto'
+import fastify from 'fastify'
+import { knex } from './database'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Test Transaction',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running')
+  })
