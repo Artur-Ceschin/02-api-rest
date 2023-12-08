17d7e0573b73a979efd165c6a045ed82c13d24c9
Artur Ceschin <artur.ceschin@gmail.com>
2023-12-08 16:02:54 -0300

    fix: 🐛 minor fix and recotors

--- src/database.ts
+++ src/database.ts
@@ -1,16 +1,14 @@
 import { Knex, knex as setupKnex } from 'knex'
 import { env } from './env'
 
-const connectionService =
-  env.DATABASE_CLIENT === 'sqlite'
-    ? {
-        filename: env.DATABASE_CLIENT,
-      }
-    : env.DATABASE_CLIENT
-
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
-  connection: connectionService,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_CLIENT,
+        }
+      : env.DATABASE_CLIENT,
 
   useNullAsDefault: true,
   migrations: {
